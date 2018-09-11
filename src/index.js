f5106804ad3c92f6d349c963035b02ee9ace3ae6
artkon <artemkonopelko@gmail.com>
2018-09-11 19:07:10 +0300

    implement getZerosCount function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+
+  var maxPow = 0;
+  var max = Math.pow(10, 8);
+
+  //Вычисляем максимальную степень 5 которая влазит в number
+  for(var i = 1; i <= max ; i++){
+    if( number / Math.pow(5, i) >= 1 ){
+      maxPow = i;    
+    } else {
+      break;
+    }
+  }
+
+  var result = 0;
+
+  for(var i = 1; i <= maxPow; ++i){
+    result += Math.floor( number / Math.pow(5, i) );
+  }
+  
+  return result;
 }
